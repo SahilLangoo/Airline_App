5074c3fef3bc59ebf9b3d2b1b619d7cfd06836cd
Sahil <sahilahmed3066@gmail.com>
2020-07-08 18:11:42 +0530

    fixed login

--- users/views.py
+++ users/views.py
@@ -12,8 +12,8 @@ def index(request):
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST[" username "]
-        password = request.POST[" password "]
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -22,6 +22,8 @@ def login_view(request):
             return render(request, "users/login.html", {
                 "message": "Invalid Credentials."
             })
+    else:
+        return render(request, "users/login.html")
 
 def logout_view(request):
     logout(request)
